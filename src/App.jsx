bfbbcb218e67a87ebf6fcf55a5b3b3c3d4674368
anirudh23122 <annupandt23122@gmail.com>
2026-01-14 21:04:36 +0530

    basic done

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,15 @@ const App = () => {
          
          <input type="text" 
         placeholder = "enter notes  heading"
-        className='px-5 py-2 w-full border-2 outline-none rounded'
+        className='px-5 py-2 w-full font-medium border-2 outline-none rounded'
         />
 
       <textarea
       placeholder='enter details'
-      className='px-5 py-2 w-full border-2 h-20 outline-none rounded'
+      className='px-5 py-2 w-full font-medium border-2 h-20 outline-none rounded'
       /> 
 
-      <button className='bg-white text-black w-full px-5 py-2 outline-none rounded'>Add Notes</button>
+      <button className='bg-white text-black font-medium w-full px-5 py-2 outline-none rounded'>Add Notes</button>
        </div>
       
       </form>
