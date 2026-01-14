a9719c11dec48183085debdf5f0bdd320ae11548
anirudh23122 <annupandt23122@gmail.com>
2026-01-14 19:19:58 +0530

    notes

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <form className='flex justify-between'>
-        <input type="text" placeholder = "enter value"/>
-      <input type="text" placeholder='enter details'/> 
+    <div className='h-screen bg-black text-white'>
+      <form className='flex p-10 justify-between items-start gap-4'>
+       <div className='flex p-10 w-1/2 items-start flex-col gap-4'>
+         <input type="text" 
+        placeholder = "enter notes  heading"
+        className='px-5 py-2 border-2 rounded'
+        />
+      <input type="text" 
+      placeholder='enter details'
+      className='px-5 py-2  border-2 h-20 rounded'
+      /> 
+      <button className='bg-white text-black px-5 py-2 rounded'>Add Notes</button>
+       </div>
+      <img className='h-60' src="https://gallery.yopriceville.com/downloadfullsize/send/14426" alt="" />
       </form>
+      
     </div>
   )
 }
