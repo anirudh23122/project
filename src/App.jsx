4d66397f81b44c4dc022919c3d299a64d300971f
anirudh23122 <annupandt23122@gmail.com>
2026-01-15 16:28:54 +0530

    two day binded

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+const[title, setTitle] = useState('')
+const[detail, setDetail] = useState('')
 const submitHandler =(e) => {
 e.preventDefault()
-console.log("form submitted");
-
+console.log(title, detail);
+setTitle('')
+setDetail('')
 }
 
   return (
@@ -16,14 +19,22 @@ console.log("form submitted");
       className='flex p-10 lg:w-1/2 gap-4 flex-col items-start '>
         <h1 className='text-3xl font-bold'>Add Notes</h1>
          
-         <input type="text" 
+        <input type="text" 
         placeholder = "enter notes  heading"
         className='px-5 py-2 w-full font-medium border-2 outline-none rounded'
+        value={title}
+        onChange={(e) =>{
+            setTitle(e.target.value)
+        }}
         />
 
       <textarea
       placeholder='enter details'
       className='px-5 py-2 w-full font-medium border-2 h-20 outline-none rounded'
+      value={detail}
+      onChange={(e) => {
+           setDetail(e.target.value)
+      }}
       /> 
 
       <button className='bg-white text-black font-medium w-full px-5 py-2 outline-none rounded'>Add Notes</button>
