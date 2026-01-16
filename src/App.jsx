085e0738b2ae43fe7b5ee7d4afafec841a3a2f85
anirudh23122 <annupandt23122@gmail.com>
2026-01-16 16:20:17 +0530

    delete fun

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+
 const App = () => {
 
 const[title, setTitle] = useState('')
@@ -15,6 +16,10 @@ setTask(isTask)
 
 setTitle('')
 setDetail('')
+}
+const deleteNote =(idx) =>{
+    const copyTask = [...task]
+    copyTask.splice()
 }
 
   return (
@@ -52,11 +57,16 @@ setDetail('')
 
       <div className=' lg:w-1/2 lg:border-l-2 p-10'>
       <h1 className='text-3xl font-bold'>Recent Notes</h1>
-       <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-full overflow-auto'>
+       <div className='flex flex-wrap items-start justify-start gap-5 mt-5 h-[90%] overflow-auto'>
         {task.map(function(elem,idx){
-         return <div key={idx} className='relative h-52 w-40 bg-cover rounded-xl text-black py-8 px-4 bg-[url("https://static.vecteezy.com/system/resources/thumbnails/010/793/873/small/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png")]'>
+         return <div key={idx} className='flex justify-between flex-col items-start pb-5 relative h-52 w-40 bg-cover rounded-xl text-black py-8 px-4 bg-[url("https://static.vecteezy.com/system/resources/thumbnails/010/793/873/small/a-lined-note-paper-covered-with-transparent-tape-on-a-yellow-background-with-a-white-checkered-pattern-free-png.png")]'>
+          
           <h3 className='leading-tight text-xl font-bold'>{elem.title}</h3>
           <p className='leading-snug mt-4 font-medium text-blue-900' >{elem.detail}</p>
+          <button onClick={
+            deleteNote(idx)
+          } 
+            className='w-full cursor-pointer active:scale-95 bg-red-500 text-white py-1 text-xs rounded-xl font-bold'>Delete</button>
          </div>     
         
         })}
